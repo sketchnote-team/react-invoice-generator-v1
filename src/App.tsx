0920cb905f31731097a7e424e58eb1c9614c2fdb
KavyaMenon <63591020+KavyaMenon@users.noreply.github.com>
2024-07-18 00:03:25 +0530

    fix: export

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import InvoicePage from './components/InvoicePage'
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="center fs-30">React Invoice Generator</h1>
+    
       <InvoicePage />
-    </div>
+ 
   )
 }
 
